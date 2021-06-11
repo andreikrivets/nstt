2e4c0834d26ec869aa177f350fa617c276f039ad
andreikrivets <36642792+andreikrivets@users.noreply.github.com>
2021-06-11 16:20:00 +0300

    fix: filtre logic fix

--- src/utils/getFiltredData.js
+++ src/utils/getFiltredData.js
@@ -1,11 +1,13 @@
 export default (data, tags) => {
-  const filtredData = data.filter((note) => {
-    const isValid = tags.reduce((acc, tag) => {
-      // eslint-disable-next-line no-param-reassign
-      acc += note.tags.includes(tag);
-      return acc;
-    }, false);
-    return isValid;
-  });
+  const filtredData = [];
+  for (let i = 0; i < data.length; i += 1) {
+    let counter = 0;
+    if (data[i].tags.includes(tags[0])) {
+      for (let j = 0; j < tags.length; j += 1) {
+        if (data[i].tags.includes(tags[j])) counter += 1;
+      }
+    }
+    if (counter === tags.length) filtredData.push(data[i]);
+  }
   return filtredData;
 };
