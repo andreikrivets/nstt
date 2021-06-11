3e4a4a6a6c677da85fe9dc5ea79aae254a525dc8
andreikrivets <36642792+andreikrivets@users.noreply.github.com>
2021-06-11 19:02:44 +0300

    feat: highlite tags in preview mode

--- src/components/Forms/ShowForm.jsx
+++ src/components/Forms/ShowForm.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import parse from 'html-react-parser';
 import { Card } from 'react-bootstrap';
 
 const ShowForm = ({ content }) => {
+  const highlitedText = content.text
+    ? content.text.replace(/\n$/g, '\n\n').replace(/#\w+/g, '<strong>$&</strong>')
+    : 'nothing to show';
+  const element = parse(highlitedText);
   return (
     <>
       <Card>
         <Card.Title>{content.title}</Card.Title>
-        <Card.Body>{content.text}</Card.Body>
+        <Card.Body>{element}</Card.Body>
       </Card>
     </>
   );
